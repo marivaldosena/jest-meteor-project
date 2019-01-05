847c42a845848e5d3a5b05474903a654ca8ded5b
Marivaldo Sena <msena.ifsp@gmail.com>
2019-01-05 17:43:56 -0200

    Helpers created for displaying username and email on profile page

--- client/js/profile.js
+++ client/js/profile.js
@@ -10,6 +10,24 @@ Template.profile.rendered = function() {
     inactiveLinks.forEach(link => $(link).removeClass('selected'))
 }
 
-Template.profile.events({
+Template.profile.helpers({
+    email() {
+        if (! Meteor.user()) {
+            Bert.alert('You are not logged in, permission denied',
+                'danger', 'growl-top-right')
+                return false
+        } else {
+            return Meteor.user().emails[0].address
+        }
+    },
 
+    username() {
+        if (! Meteor.user()) {
+            Bert.alert('You are not logged in, permission denied',
+                'danger', 'growl-top-right')
+                return false
+        } else {
+            return Meteor.user().username
+        }
+    }
 })
